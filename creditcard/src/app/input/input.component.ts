69759f8af209781ec5399fbd30b47b7a6aeb0154
Joanne Lin <y3933y3933@gmail.com>
2021-01-18 16:12:20 +0800

    feat: new creditcard

---
+++ creditcard/src/app/input/input.component.ts
@@ -0,0 +1,19 @@
+import { FormControl } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent implements OnInit {
+  @Input() control: FormControl;
+  @Input() label: string;
+  constructor() {}
+
+  ngOnInit(): void {}
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+    return dirty && touched && errors;
+  }
+}
